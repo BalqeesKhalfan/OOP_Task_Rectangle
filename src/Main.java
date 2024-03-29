66a514c9fbfdf9d3d63e1f4cac94bd3476f7be99
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-03-29 12:18:50 +0400

    Acess on Class Rectangle
    create object from rectangle class to claculate Area and Perimeter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+        System.out.print("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+
+        // Creating a rectangle object using constructor
+        Rectangle rect = new Rectangle(length, width);
+
+        // Accessing attributes using getter methods
+        System.out.println("\nLength: " + rect.getLength());
+        System.out.println("Width: " + rect.getWidth());
+
+        // Calculating area and perimeter using methods
+        System.out.println("Area: " + rect.calculateArea());
+        System.out.println("Perimeter: " + rect.calculatePerimeter());
+
+        scanner.close();
     }
 }
